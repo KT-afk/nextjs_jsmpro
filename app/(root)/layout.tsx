6ff5ec7a2a112d2d5c143f1f7d01108389d42406
kt_ae <kongtat@aevice.com>
2024-04-25 11:38:03 +0800

    Building a website with search

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/ui/Footer'
+import React from 'react'
+
+const layout = ({children} : {children:React.ReactNode}) => {
+  return (
+    <>
+    <Navbar />
+    {children}
+    <Footer />
+    </>
+  )
+}
+
+export default layout
